5f189ff4c7c4cc7863f4aec852fc04d68f3f4ebd
nikshrma <nikshrma2006@gmail.com>
2025-11-03 11:20:19 +0530

    defined zod schemas and initial api endpoints

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,2 +1,22 @@
 import express, { Router } from "express"
+import zod from "zod";
 export const notesRouter = Router();
+const signupSchema = zod.object({
+    username: zod.email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
+const signinSchema = zod.object({
+    username: zod.email(),
+    password: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string()
+})
+
+notesRouter.post("/signup" , (req,res)=>{
+
+})
+notesRouter.post("/signin" , (req,res)=>{
+
+})
