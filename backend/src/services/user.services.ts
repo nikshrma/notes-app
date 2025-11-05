801e215290282e3852230c86de9ad8b97d14089c
nikshrma <nikshrma2006@gmail.com>
2025-11-05 10:08:09 +0530

    added type-safety to DB logic

--- backend/src/services/user.services.ts
+++ backend/src/services/user.services.ts
@@ -1,16 +1,25 @@
 import { User } from "../config/db.config.js";
+import bcrypt from "bcrypt";
+const saltRounds = 8;
 import type { UserInputOnSignUp } from "../schemas/signup.schema.js";
+import type { UserInputOnSignIn } from "../schemas/signin.schema.js";
 
-export async function checkUserExistance(userInput : UserInputOnSignUp){
-    return await User.find({username:userInput.username});
+export async function checkUserExistance(userName : string){
+    return await User.findOne({username:userName});
 }
 
 export async function createUser(userPayload: UserInputOnSignUp){
+    const userHash = await bcrypt.hash(userPayload.password , saltRounds)
     const user = await User.create({
         username:userPayload.username,
-        password:userPayload.password,
+        password:userHash,
         firstName:userPayload.firstName,
-        fastName:userPayload.lastName
+        lastName:userPayload.lastName
     })
     return user;
 };
+
+export async function signInUser(userPayload: UserInputOnSignIn  , hash: string){
+    const a:boolean = await bcrypt.compare(userPayload.password , hash);
+    return a;
+}
